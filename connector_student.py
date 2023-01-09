91e45d51d633993c7da917060e6ed80fd7ff3790
AnteCreeper <dimon1232002@gmail.com>
2023-01-10 00:31:33 +0300

    Добавил сортировку

--- connector_student.py
+++ connector_student.py
@@ -1,19 +1,32 @@
 from PyQt5 import QtSql
-from PyQt5.QtWidgets import QTableWidget, QTableWidgetItem
+from PyQt5.QtWidgets import QTableView
+from PyQt5.QtCore import QSortFilterProxyModel
+from PyQt5.QtGui import QStandardItem, QStandardItemModel
 
 
-class TableStudent(QTableWidget):
+class TableStudent(QTableView):
     def __init__(self):
         super().__init__()
-        self.clear()
+        model = QStandardItemModel()
         query = QtSql.QSqlQuery("""SELECT fullname, squad, course FROM students""")
-        self.setColumnCount(3)
-        self.setHorizontalHeaderLabels(["ФИО", "Группа", "Курс"])
+        model.setColumnCount(3)
+        model.setHorizontalHeaderLabels(["ФИО", "Группа", "Курс"])
+        proxy = MySortFilterProxyModel()
+        proxy.setSourceModel(model)
+        self.setModel(proxy)
+        self.setSortingEnabled(True)
         while query.next():
-            rows = self.rowCount()
-            self.setRowCount(rows + 1)
-            self.setItem(rows, 0, QTableWidgetItem(query.value(0)))
-            self.setItem(rows, 1, QTableWidgetItem(query.value(1)))
-            self.setItem(rows, 2, QTableWidgetItem(str(query.value(2))))
+            rows = model.rowCount()
+            model.setRowCount(rows + 1)
+            model.setItem(rows, 0, QStandardItem(query.value(0)))
+            model.setItem(rows, 1, QStandardItem(query.value(1)))
+            model.setItem(rows, 2, QStandardItem(str(query.value(2))))
         self.resizeColumnsToContents()
-        self.setFixedWidth(442)
+
+
+class MySortFilterProxyModel(QSortFilterProxyModel):
+    def lessThan(self, source_left, source_right):
+        if (source_left.isValid() and source_right.isValid()):
+            if (source_left.column() == 2):  # <== номер колонки с числами
+                return int(source_left.data()) < int(source_right.data())
+        return super(MySortFilterProxyModel, self).lessThan(source_left, source_right)
